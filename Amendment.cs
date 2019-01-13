6942a80cbc854168b973530aa9c568f02df363d5
wilsonalwaris <wilsonalwaris@gmail.com>
2019-01-13 18:02:37 +0000

    Initial commit

---
+++ Amendment.cs
@@ -0,0 +1,44 @@
+﻿namespace LiskovsExample
+{
+    /// <summary>
+    /// Amendment process implementation
+    /// </summary>
+    /// <seealso cref="LiskovsExample.MimumGroupSize" />
+    public class Amendment : MimumGroupSize
+    {
+        /// <summary>
+        /// Amends this booking
+        /// </summary>
+        public void Amend(string booking)
+        {
+            // implementation to amend a booking.
+
+            // after amendment check if minimum group size is breached.
+            this.IsMinumGroupSizeBreached(9);
+        }
+
+        /// <summary>
+        /// Determines whether [is minum group size breached] [the specified current group size].
+        /// </summary>
+        /// <param name="currentGroupSize">Size of the current group.</param>
+        /// <returns>
+        ///   <c>true</c> if [is minum group size breached] [the specified current group size]; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsMinumGroupSizeBreached(int currentGroupSize)
+        {
+            // please note the condition is changed but that is ok we are still not breaching LSP
+            if (currentGroupSize < 10)
+            {
+                /* however the base class expects this event to be noted in the booking which this implementation is not 
+                 this leads to different behaviour and maintenance issue hence LSP breach.*/
+                return true;
+
+                /* the above code smell is caused due to incorrect use of abstract class virutal method
+                  what should have been done is the noting the implemtation to booking should have been made mandatory for all 
+                  deriving classes. An interface and a derived base class that is inherted by other interface implementing classes comes to mind*/
+            }
+
+            return false;
+        }
+    }
+}
---
+++ MimumGroupSize.cs
@@ -0,0 +1,37 @@
+﻿namespace LiskovsExample
+{
+    /// <summary>
+    /// The base mimum group size implementation
+    /// </summary>
+    public abstract class MimumGroupSize
+    {
+        /// <summary>
+        /// Determines whether [is minum group size breached] [the specified current group size].
+        /// </summary>
+        /// <param name="currentGroupSize">Size of the current group.</param>
+        /// <returns>
+        ///   <c>true</c> if [is minum group size breached] [the specified current group size]; otherwise, <c>false</c>.
+        /// </returns>
+        public virtual bool IsMinumGroupSizeBreached(int currentGroupSize)
+        {
+            // if the group size is less than 20 its a breach.
+            if (currentGroupSize < 20)
+            {
+                // make a note in the booking
+                this.MakeaNote("this.Booking");
+                return true;
+            }
+            
+            return false;
+        }
+
+        /// <summary>
+        /// Makeas the note.
+        /// </summary>
+        /// <param name="booking">The booking.</param>
+        public void MakeaNote(string booking)
+        {
+            // made a note
+        }
+    }
+}
